06355e1b0fda0362a69e0839dd235acc1eb10947
Alptuğ Doğan <alpdogan000@gmail.com>
2023-03-22 16:35:27 +0300

    Add response DTO for teams

---
+++ src/main/java/com/alpdogan/FootballTeamsTestCaseForPiA/dto/response/TeamResponseDto.java
@@ -0,0 +1,14 @@
+package com.alpdogan.FootballTeamsTestCaseForPiA.dto.response;
+
+import lombok.Data;
+
+@Data
+public class TeamResponseDto {
+
+    private int id;
+
+    private String teamName;
+
+    private int playerIds;
+
+}
