688fa9d429334a9c89c3f67f6c2d5703ded953a0
Alptuğ Doğan <alpdogan000@gmail.com>
2023-03-22 16:28:05 +0300

    Add DTO for updating teams

---
+++ src/main/java/com/alpdogan/FootballTeamsTestCaseForPiA/dto/UpdateTeamRequestDto.java
@@ -0,0 +1,12 @@
+package com.alpdogan.FootballTeamsTestCaseForPiA.dto;
+
+import lombok.Data;
+
+@Data
+public class UpdateTeamRequestDto {
+
+    private int id;
+
+    private String teamName;
+
+}
