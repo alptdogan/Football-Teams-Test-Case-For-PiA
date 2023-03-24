17a2fe2e656049ab5a7e0f0345a8116cb7b35453
Alptuğ Doğan <alpdogan000@gmail.com>
2023-03-24 15:46:16 +0300

    Update TeamService, refactoring for the last time

--- src/main/java/com/alpdogan/FootballTeamsTestCaseForPiA/service/TeamService.java
+++ src/main/java/com/alpdogan/FootballTeamsTestCaseForPiA/service/TeamService.java
@@ -1,12 +1,9 @@
 package com.alpdogan.FootballTeamsTestCaseForPiA.service;
 
-import com.alpdogan.FootballTeamsTestCaseForPiA.dto.request.SavePlayerRequestDto;
 import com.alpdogan.FootballTeamsTestCaseForPiA.dto.request.SaveTeamRequestDto;
 import com.alpdogan.FootballTeamsTestCaseForPiA.dto.request.UpdateTeamRequestDto;
 import com.alpdogan.FootballTeamsTestCaseForPiA.dto.response.TeamResponseDto;
-import com.alpdogan.FootballTeamsTestCaseForPiA.entity.Player;
 import com.alpdogan.FootballTeamsTestCaseForPiA.entity.Team;
-import com.alpdogan.FootballTeamsTestCaseForPiA.repository.PlayerRepository;
 import com.alpdogan.FootballTeamsTestCaseForPiA.repository.TeamRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +12,6 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 @Service
 public class TeamService {
@@ -26,10 +22,6 @@ public class TeamService {
     @Autowired
     ModelMapper modelMapper;
 
-    @Autowired
-    PlayerRepository playerRepository;
-
-    //just trying out limiting players etc.
     public String saveTeam(SaveTeamRequestDto saveTeamRequestDto) {
 
         Team team = modelMapper.map(saveTeamRequestDto, Team.class);
@@ -87,40 +79,5 @@ public class TeamService {
         return "Team Deleted.";
 
     }
-
-
-    /*
-    //updateTeam?
-    public String addPlayerToTheTeam(Team team, Player player) {
-
-        int teamId = team.getId();
-
-        Optional<Team> teamOptional = teamRepository.findById(teamId);
-        Team team1 = teamOptional.get();
-
-        if (team1.getPlayers().size() == 18) {
-
-            return team1.getTeamName() + " Has Already 18 Players, Cannot Add More.";
-
-        } else if (player.isGoalkeeper() == true && team1.getGoalkeepers().length == 2) {
-
-            return team1.getTeamName() + " Has Already 2 Goalkeepers, Cannot Add More.";
-
-        } else if (player.isForeigner() == true && team1.getForeigners().length == 6) {
-
-            return team1.getTeamName() + " Has Already 6 Foreign Players, Cannot Add More.";
-
-        } else {
-
-            team = teamRepository.save(team1);
-
-            return "Changes Has Been Saved Successfully.";
-
-        }
-
-    }
-     */
-
-
 
 }
