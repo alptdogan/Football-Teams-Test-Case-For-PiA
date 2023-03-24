702594723e8e3a590a4cf7e5089f4ec6d89e7c40
Alptuğ Doğan <alpdogan000@gmail.com>
2023-03-24 13:48:00 +0300

    Update SavePlayerRequestDto

--- src/main/java/com/alpdogan/FootballTeamsTestCaseForPiA/dto/request/SavePlayerRequestDto.java
+++ src/main/java/com/alpdogan/FootballTeamsTestCaseForPiA/dto/request/SavePlayerRequestDto.java
@@ -7,8 +7,10 @@ public class SavePlayerRequestDto {
 
     private String fullName;
 
-    private boolean isForeigner;
+    //private boolean isForeigner;
 
-    private boolean isGoalkeeper;
+    //private boolean isGoalkeeper;
+
+    private int teamId;
 
 }
