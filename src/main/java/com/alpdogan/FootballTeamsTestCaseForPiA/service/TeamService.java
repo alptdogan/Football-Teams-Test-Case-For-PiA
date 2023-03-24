69043eaca8078d7aa8f0ac0ce720793cd802293b
Alptuğ Doğan <alpdogan000@gmail.com>
2023-03-24 12:45:44 +0300

    Update TeamService add method that saves teams

--- src/main/java/com/alpdogan/FootballTeamsTestCaseForPiA/service/TeamService.java
+++ src/main/java/com/alpdogan/FootballTeamsTestCaseForPiA/service/TeamService.java
@@ -1,9 +1,14 @@
 package com.alpdogan.FootballTeamsTestCaseForPiA.service;
 
+import com.alpdogan.FootballTeamsTestCaseForPiA.dto.request.SaveTeamRequestDto;
+import com.alpdogan.FootballTeamsTestCaseForPiA.entity.Player;
+import com.alpdogan.FootballTeamsTestCaseForPiA.entity.Team;
 import com.alpdogan.FootballTeamsTestCaseForPiA.repository.TeamRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.stream.Stream;
 
 @Service
 public class TeamService {
@@ -13,6 +18,27 @@ public class TeamService {
 
     @Autowired
     ModelMapper modelMapper;
+
+    //just trying out limiting players etc.
+    public String saveTeam(SaveTeamRequestDto saveTeamRequestDto) {
+
+        Team team = modelMapper.map(saveTeamRequestDto, Team.class);
+
+        //team.getPlayers().stream().limit(18);
+
+        if (team.getPlayers().size() >= 19) {
+
+            return team.getTeamName() + " Has Already 18 Players, Cannot Add More.";
+
+        } else {
+
+            team = teamRepository.save(team);
+
+            return team.getTeamName() + " Has Been Created Successfully.";
+
+        }
+
+    }
 
 
 
